621641a02efbd9286d555ae61cb482e0c18fff2f
ill-usion <aalhadrami7@gmail.com>
2024-01-14 18:53:52 +0400

    added missing docs

--- src/config.rs
+++ src/config.rs
@@ -5,9 +5,12 @@ use dot_properties_parser::{parse_properties_file, PropertyValue};
 /// Minecraft 1.16.5 protocol version
 pub const PROTOCOL_VERSION: i32 = 754;
 
+/// Key value pairs containing the server configuration
 pub struct ServerConfig(HashMap<String, PropertyValue>);
 
 impl ServerConfig {
+    // TODO: add better error handling than `Box<dyn Error>`
+    /// Parses a `server.properties` file
     pub fn from_file(path: &str) -> Result<ServerConfig, Box<dyn Error>> {
         let path = Path::new(path);
         parse_properties_file(path, None).map(ServerConfig)
--- src/errors.rs
+++ src/errors.rs
@@ -1,29 +1,38 @@
 use thiserror::Error;
 
+/// Errors caused by the Minecraft client
 #[derive(Error, Debug)]
 pub enum ClientError {
+    /// Excluding this one
     #[error("another instance of client is already running")]
     AlreadyRunning,
+    /// Client provided an invalid packet ID
     #[error("invalid packet id `{0}`")]
     InvalidPacketID(i32),
+    /// Client provided a server host different than the one configured in server.properties
     #[error("packet contains an incorrect server host value `{0}`")]
     IncorrectHost(String),
+    /// Client is on a different Minecraft version
     #[error("invalid protocol version `{0}`, expected `{1}`")]
     InvalidProtocolVersion(i32, i32),
+    /// Packet parsing error, for now
     #[error("error parsing packet: {0}")]
     PacketError(String),
 }
 
 #[derive(Error, Debug)]
 pub enum ServerError {
+    /// Required property in server configuration is missing
     #[error("property `{0}` is missing")]
     MissingProperty(String),
+    /// Binding to socket failed
     #[error("binding socket to host `{0}` port `{1}` failed: {2}")]
     SocketBindFailed(String, u16, String),
 }
 
 #[derive(Error, Debug)]
 pub enum PacketError {
+    /// Expected a field of a certain type
     #[error("expected field `{0}`")]
     ExpectedField(String),
 }
